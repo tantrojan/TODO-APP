f07abedcfaf434be65a8e4bfa0e61310e18a8083
tantrojan <tg.99ghosh@gmail.com>
2018-05-23 00:57:08 +0530

    NPM initialised

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(express.static('static'));
+
+app.set('view engine','ejs');
+
+app.use("/","./routes/list");
+
+app.listen(3000, function(){
+	console.log("Listening to port 3000");
+});
