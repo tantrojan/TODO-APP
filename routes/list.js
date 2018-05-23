b44915dd2100f3b1ee6728d4987f122b6bf9930d
tantrojan <tg.99ghosh@gmail.com>
2018-05-24 00:40:19 +0530

    Complete with Static Data

--- routes/list.js
+++ routes/list.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-
+var input_list = [];
 router.get("/",function(req,res){
 	res.render("todo.ejs",
 	{	title : ' TODO Application ',
 		name : 'Tanmoy ',
-		todo_list : ['Learning node.js', 'Doing User Authentication', 'Building full BLog Application'],
+		todo_list : input_list
 	});
-	
+
 });
 
-// router.post("/",function(req,res){
+router.post("/",function(req,res){
+	console.log(req.body);
+	input_list.push(req.body.item);
+	res.render("todo.ejs",
+	{	title : ' TODO Application ',
+		name : 'Tanmoy ',
+		todo_list : input_list
+	});
+});
 
-// });
 
-// router.put("/",function(req,res){
+router.delete("/:item",function(req,res){
+	console.log("Delete Request Received");
+	input_list = input_list.filter(function(todo){
+		return todo.replace(/ /g, '-') !== req.params.item;
+	});
+	console.log(input_list);
+	console.log(req.params.item);
+	res.json(input_list); // this is received by AJAX
 
-// });
-
-// router.delete("/",function(req,res){
-
-// });
+});
 
 module.exports = router ;
--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,21 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+
+// TASK DIV ONCLICK
+$(document).ready(function(){
+    console.log("JQUERY WORKING");
+
+    $('.task').on('click',function(){
+        var item = $(this).text().replace(/ /g, "-");
+        item = item.slice(0, -1); // to remove the last - {hyphen}
+        $.ajax({
+            type: 'DELETE',
+            url: '/' + item ,
+            success: function(data){
+                console.log("deletion Successful");
+                window.location.href = window.location.href;
+            }
+        });
+    });
+});
