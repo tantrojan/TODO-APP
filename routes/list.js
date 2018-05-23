2cd1043a00ba162a010927a40f9f4f8dba9fbe14
tantrojan <tg.99ghosh@gmail.com>
2018-05-24 01:26:21 +0530

    Name prompt Added

--- routes/list.js
+++ routes/list.js
@@ -1,24 +1,46 @@
 const express = require("express");
 const router = express.Router();
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 var input_list = [];
+var name = '';
+
 router.get("/",function(req,res){
-	res.render("todo.ejs",
-	{	title : ' TODO Application ',
-		name : 'Tanmoy ',
-		todo_list : input_list
-	});
+	if(name === '')
+	{
+		res.render('namereq.ejs');
+	}
+	else
+	{
+		res.render("todo.ejs",
+		{	name : name,
+			todo_list : input_list
+		});
+	}
 
 });
 
 router.post("/",function(req,res){
-	console.log(req.body);
-	input_list.push(req.body.item);
-	res.render("todo.ejs",
-	{	title : ' TODO Application ',
-		name : 'Tanmoy ',
-		todo_list : input_list
-	});
+	if(name==='')
+	{
+		name = req.body.name;
+		res.render("todo.ejs",
+		{	name : name,
+			todo_list : input_list
+		});
+	}
+	else{
+		input_list.push(req.body.item);
+		res.render("todo.ejs",
+		{	name : name,
+			todo_list : input_list
+		});
+	}
 });
 
 
