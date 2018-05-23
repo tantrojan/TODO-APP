2cb13a5330cb39eb032700c0c8b9c0c7d1771172
tantrojan <tg.99ghosh@gmail.com>
2018-05-23 19:29:46 +0530

    Clock added

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+//BODY PARSER
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+
+//STATIC
 app.use(express.static('static'));
 
+//VIEW ENGINE
 app.set('view engine','ejs');
 
+//ROUTES
 app.use("/",require("./routes/list"));
 
-app.listen(3000, function(){
-	console.log("Listening to port 3000");
+app.listen(8080, function(){
+	console.log("Listening to port 8080");
 });
--- routes/list.js
+++ routes/list.js
@@ -3,19 +3,24 @@ const router = express.Router();
 
 
 router.get("/",function(req,res){
-	res.render("todo",{title : ' TODO Application '});
+	res.render("todo.ejs",
+	{	title : ' TODO Application ',
+		name : 'Tanmoy ',
+		todo_list : ['Learning node.js', 'Doing User Authentication', 'Building full BLog Application'],
+	});
+	
 });
 
-router.post("/",function(req,res){
+// router.post("/",function(req,res){
 
-});
+// });
 
-router.put("/",function(req,res){
+// router.put("/",function(req,res){
 
-});
+// });
 
-router.delete("/",function(req,res){
+// router.delete("/",function(req,res){
 
-});
+// });
 
 module.exports = router ;
